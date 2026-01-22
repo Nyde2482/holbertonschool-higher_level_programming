eb112353b1c93c29bd72812729dfaddbdb4a8d14
Nyde2482 <Callejoesteban@gmail.com>
2026-01-22 13:26:09 +0000

    feat : ajouter la fonction safe_print_list pour imprimer en toute sécurité les éléments d'une liste

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        newList = []
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    except IndexError:
+        pass
+    print()
+    return count
+
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
