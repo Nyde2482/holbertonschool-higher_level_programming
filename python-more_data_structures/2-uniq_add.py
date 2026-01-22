2b7c0616355f1e01c7ba645e9462623acd425a4c
Nyde2482 <Callejoesteban@gmail.com>
2026-01-22 13:13:31 +0000

    feat : ajouter les fonctions uniq_add et common_elements pour gérer les listes et les ensembles

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_list = []
+    for element in my_list:
+        if element not in new_list:
+            new_list.append(element)
+    return sum(new_list)
