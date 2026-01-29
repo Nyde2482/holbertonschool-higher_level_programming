abb6e45853cc4bb2a3f4f6ab21e9aaf97a9f9e4f
Nyde2482 <Callejoesteban@gmail.com>
2026-01-29 22:01:37 +0000

    feat : améliorer la documentation et corriger le nom du paramètre dans le constructeur de la classe Square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Square module."""
+
+
 class Square:
-    def __init__(self, __size):
-        self.__size = __size
+    """Square with private size."""
+
+    def __init__(self, size):
+        self.__size = size
